01c7633a2349081bf8c2b9d205f3d59ea731939b
Alifoxy <nightfoxy666@gmail.com>
2024-02-01 19:45:49 +0300

    hw1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // -------------------------Homework1---------------------------
-// simpsons project run
+//simpsons project run
 import {App} from './HW1/simpsons/Simpsons';
-//
+
 // rick & morty project run
 // import {App} from './HW1/rick_&_morty/Characters';
 // -------------------------------------------------------------
