84e454059b151583b82f7e9b7c79e692cb3bc55f
Alifoxy <nightfoxy666@gmail.com>
2024-02-06 20:14:59 +0300

    hw1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 //-------------------------Homework2---------------------------
 //users project run
-// import {App} from './components/homework2/users/RunApp';
+import {App} from './components/homework2/users/RunApp';
 
 //spacex project run
-import {App} from './components/homework2/spacex/RunApp';
+// import {App} from './components/homework2/spacex/RunApp';
 //-------------------------------------------------------------
 
-export{App};
+export {App}
+
