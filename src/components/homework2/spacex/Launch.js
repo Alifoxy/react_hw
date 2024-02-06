bbd4ac5892649816014c44edc99d4b66260b771f
Alifoxy <nightfoxy666@gmail.com>
2024-02-06 19:56:57 +0300

    hw1

--- src/components/homework2/spacex/Launch.js
+++ src/components/homework2/spacex/Launch.js
@@ -1,12 +1,12 @@
 import './style/RunAppCss.css';
 export const Launch = ({launch}) => {
-    const {mission_name, launch_year, mission_patch_small} = launch;
+    const {mission_name, launch_year, links:{mission_patch_small}} = launch;
     if (launch_year < 2020 || launch_year > 2020){
         return (
             <div className={'launch_block'}>
                 <div>mission_name: {mission_name}</div>
                 <div>launch_year: {launch_year}</div>
-                <div>mission_patch_small: {mission_patch_small}</div>
+                <img src={mission_patch_small} alt={'mission_patch'}/>
             </div>
         );
     }
