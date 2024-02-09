b8972b9e0083c82ae4c1b7dd59d1a86f30915daa
Alifoxy <nightfoxy666@gmail.com>
2024-02-09 18:11:25 +0300

    hw3

--- src/axios/axiosConfig.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-import { baseURL } from "../api/endpoints";
-
-export const axiosRequest = axios.create({baseURL})
--- src/constants/urls.js
+++ src/constants/urls.js
@@ -11,5 +11,3 @@ export const urls = {
     launches,
     posts,
 }
-
-
