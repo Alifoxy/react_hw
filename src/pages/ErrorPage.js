f76c5791dc3f5f5c1c5bbbdc7b388146d0c404ec
Alifoxy <nightfoxy666@gmail.com>
2024-03-13 14:18:52 +0300

    hw3

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -2,7 +2,7 @@ const ErrorPage = () => {
     return (
         <div>
             <div className={'error_block'}>
-                <h1>Error: Page not found :(</h1>
+                <h1>Error: Page was not found :(</h1>
             </div>
         </div>
     );
